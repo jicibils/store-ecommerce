06d09b951bbf895148f35d764a2e4c8c37d12710
jcibils <jicibils@hotmail.com>
2025-05-07 18:50:18 +0200

    Add function to upload images

---
+++ src/lib/uploadImage.ts
@@ -0,0 +1,23 @@
+// src/lib/uploadImage.ts
+import { supabase } from "./supabase";
+
+export async function uploadImage(file: File) {
+  const fileExt = file.name.split(".").pop();
+  const fileName = `${Date.now()}.${fileExt}`;
+  const filePath = `${fileName}`;
+
+  const { data, error } = await supabase.storage
+    .from("products")
+    .upload(filePath, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  if (error) throw error;
+
+  const { data: publicUrlData } = supabase.storage
+    .from("products")
+    .getPublicUrl(filePath);
+
+  return publicUrlData.publicUrl;
+}
