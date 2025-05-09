3c78d2dee76198e3296ab817909e7517febde5f2
jcibils <jicibils@hotmail.com>
2025-05-09 15:36:21 +0200

    Add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const APP_NAME = "Verdulería Store";
+export const LOGO_EMOJI = "🍅";
+
+export const CONTACT_EMAIL = "hola@verduleriastore.com";
+export const CONTACT_PHONE = "5491134567890"; // sin + ni espacios para WhatsApp
+export const CONTACT_ADDRESS = "Calle Falsa 123, Buenos Aires";
+
+export const SOCIAL_LINKS = {
+  instagram: "https://instagram.com/verduleriastore",
+  facebook: "https://instagram.com/verduleriastore", // si está vacío, no se muestra
+  twitter: "https://instagram.com/verduleriastore",
+};
