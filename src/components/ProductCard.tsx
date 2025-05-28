f28e804c12adf4d5b1f076f978288ce007a79785
jcibils <jicibils@hotmail.com>
2025-05-28 12:41:11 +0200

    ft: Capitalize product names

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,6 +3,7 @@
 
 import { Product } from "@/types/Product";
 import Image from "next/image";
+import capitalize from "lodash.capitalize";
 import ProductDetailsSheet from "./ProductDetailsSheet";
 
 export default function ProductCard({ product }: { product: Product }) {
@@ -26,7 +27,7 @@ export default function ProductCard({ product }: { product: Product }) {
 
       <div className="p-4 flex flex-col gap-2 flex-1">
         <h3 className="text-base font-semibold leading-tight">
-          {product.name}
+          {capitalize(product.name)}
         </h3>
         <p className="text-lg font-bold text-primary">
           ${product.price.toLocaleString()}
