facc4eb95e540077f1c05436d0d6947a7c8310f8
jcibils <jicibils@hotmail.com>
2025-05-29 14:04:23 +0200

    ft: Add new packages and schemas

---
+++ db/schema_inquiries.sql
@@ -0,0 +1,12 @@
+
+-- db/schema_inquires.sql
+
+
+-- Tabla de consultas
+CREATE TABLE IF NOT EXISTS inquiries (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT,
+    email TEXT,
+    message TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
