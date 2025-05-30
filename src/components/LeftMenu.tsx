4c7e78954e603b6bc2b6d4010c022727c5fd2054
jcibils <jicibils@hotmail.com>
2025-05-30 14:37:04 +0200

    ft: add leftMenu component

---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,53 @@
+// src/components/LeftMenu.tsx
+"use client";
+import Link from "next/link";
+import { Home, LayoutDashboard, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetTitle,
+  SheetTrigger,
+  SheetContent,
+} from "@/components/ui/sheet";
+import Logo from "./Logo";
+
+export default function LeftMenu() {
+  const navLinks = [
+    { label: "Inicio", href: "/", icon: <Home className="w-5 h-5" /> },
+    {
+      label: "Admin",
+      href: "/admin",
+      icon: <LayoutDashboard className="w-5 h-5" />,
+    },
+  ];
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className="hover:text-primary transition" aria-label="Menu">
+          <Menu className="w-6 h-6" />
+        </button>
+      </SheetTrigger>
+      <SheetContent side="left" className="w-64 p-0">
+        <div className="flex items-center justify-between p-4 border-b">
+          <SheetTitle className="text-lg font-bold flex">
+            <Logo />
+            <span className="pt-2">Menú</span>
+          </SheetTitle>
+        </div>
+        <ul className="flex flex-col">
+          {navLinks.map(({ label, href, icon }) => (
+            <li key={href}>
+              <Link
+                href={href}
+                className="flex items-center gap-2 px-4 py-3 hover:bg-muted text-sm"
+              >
+                {icon}
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+}
