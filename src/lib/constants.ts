395fe68da6f5be42e9e33030e7dde88d078dd641
jcibils <jicibils@hotmail.com>
2025-05-30 14:34:05 +0200

    ft: add constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,12 +1,14 @@
-export const APP_NAME = "Verdulería Store";
+export const APP_NAME = "Fruvercom";
 export const LOGO_EMOJI = "🍅";
+export const LOGO = "/fruvercomLogo.png";
 
-export const CONTACT_EMAIL = "hola@verduleriastore.com";
-export const CONTACT_PHONE = "5491134567890"; // sin + ni espacios para WhatsApp
-export const CONTACT_ADDRESS = "Calle Falsa 123, Buenos Aires";
+export const CONTACT_EMAIL = "fruvercom.tienda@gmail.com";
+export const CONTACT_PHONE = "5493586548002"; // sin + ni espacios para WhatsApp
+export const CONTACT_ADDRESS = "Calle La madrid 1311, Río Cuarto";
+export const WORKING_HOURS = "9:00 a 12:30 y 17:00 a 21:00";
 
 export const SOCIAL_LINKS = {
-  instagram: "https://instagram.com/verduleriastore",
-  facebook: "https://instagram.com/verduleriastore", // si está vacío, no se muestra
-  twitter: "https://instagram.com/verduleriastore",
+  instagram: "https://www.instagram.com/fru.ver.com23",
+  facebook: "", // si está vacío, no se muestra
+  twitter: "",
 };
