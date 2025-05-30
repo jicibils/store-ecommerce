88d9022ee2b5b17b4c972ac1c7985d67385cdc36
jcibils <jicibils@hotmail.com>
2025-05-30 14:36:48 +0200

    ft: add logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+// src/components/Logo.tsx
+"use client";
+
+import { LOGO } from "@/lib/constants";
+import Image from "next/image";
+
+export default function Logo({ size = 40 }: { size?: number }) {
+  return (
+    <Image
+      src={LOGO}
+      alt="Logo Verdulería"
+      width={size}
+      height={size}
+      className="rounded-full"
+    />
+  );
+}
