cf152ea411831514b703196650004e657a840fe2
jcibils <jicibils@hotmail.com>
2025-06-06 11:44:46 +0200

    fix: address name

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const LOGO = "/fruvercomLogo.png";
 
 export const CONTACT_EMAIL = "fruvercom.tienda@gmail.com";
 export const CONTACT_PHONE = "5493586548002"; // sin + ni espacios para WhatsApp
-export const CONTACT_ADDRESS = "Calle La madrid 1311, Río Cuarto";
+export const CONTACT_ADDRESS = "Calle Lamadrid 1311, Río Cuarto";
 export const WORKING_HOURS = "9:00 a 12:30 y 17:00 a 21:00";
 
 /** DATOS TRANSFERENCIA */
