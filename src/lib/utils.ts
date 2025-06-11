ea7a15a3b3e60a999de7c544d278e9802fb99574
jcibils <jicibils@hotmail.com>
2025-06-11 14:08:20 +0200

    ft: remove paid status

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,21 @@ export function cn(...inputs: ClassValue[]) {
 export const getStatusLabelFromList = (status: string): string => {
   return ORDER_STATUSES.find((s) => s.value === status)?.label || status;
 };
+
+export function normalizePhone(phone: string): string {
+  // Elimina todo lo que no sea número
+  let cleaned = phone.replace(/\D/g, "");
+
+  // Si empieza con 0, lo sacamos
+  if (cleaned.startsWith("0")) {
+    cleaned = cleaned.slice(1);
+  }
+
+  // Si empieza con 15, lo sacamos (clásico de celulares en Argentina)
+  if (cleaned.startsWith("15")) {
+    cleaned = cleaned.slice(2);
+  }
+
+  // Agregamos prefijo internacional (Argentina por default)
+  return `549${cleaned}`;
+}
