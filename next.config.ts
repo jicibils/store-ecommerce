9b3f3330618d35520815dc4eadd5980110336dde
jcibils <jicibils@hotmail.com>
2025-06-11 17:21:43 +0200

    fix:     unoptimized: true

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["fastly.picsum.photos", "nqfxtmajlhhiyvtnrqgi.supabase.co"], // acá sumás cualquier dominio externo que uses
+    unoptimized: true,
   },
 };
 
