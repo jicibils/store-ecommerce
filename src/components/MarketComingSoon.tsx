9d9bd7fe27051521a1edbc2e193d3d4de6cb6d51
jcibils <jicibils@hotmail.com>
2025-06-25 19:12:03 +0200

    ft: update market

--- src/components/MarketComingSoon.tsx
+++ src/components/MarketComingSoon.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function MarketComingSoon() {
   return (
-    <div className="text-center mt-12">
+    <div className="text-center mt-12 relative z-1">
       <div className="flex justify-center mb-6">
         <Image
           src="/coming_soon.png"
