18f5baa7afaccd4b15a83dc9ecb410be0636e0e6
jcibils <jicibils@hotmail.com>
2025-06-30 12:55:25 +0200

    ft: styles

--- src/components/GlobalSearch.tsx
+++ src/components/GlobalSearch.tsx
@@ -28,7 +28,7 @@ export default function GlobalSearchBar() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Buscar productos..."
-          className="w-full pl-10 pr-4 py-2 rounded-full border border-gray-300 bg-white text-gray-800 placeholder-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent transition duration-200"
+          className="w-full pl-10 pr-4 py-2 rounded-full border-2 border-orange-700 bg-white text-gray-800 placeholder-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-800 focus:border-transparent transition duration-200"
         />
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
       </div>
